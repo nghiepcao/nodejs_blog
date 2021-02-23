106f123e96b976f513cd84e51ca540d7722f97e7
unknown <nghiep975@gmail.com>
2021-02-24 00:06:34 +0700

    Delete course with some jqurey

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -44,6 +44,12 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
+    //[DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController;
